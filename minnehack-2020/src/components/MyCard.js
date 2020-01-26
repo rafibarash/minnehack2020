49fc49e2da742105971ad8521aad8d3ac107a3f3
Truman Tyson <tyson051@umn.edu>
2020-01-26 11:31:23 -0600

    began styling the cards, images are now loaded

--- minnehack-2020/src/components/MyCard.js
+++ minnehack-2020/src/components/MyCard.js
@@ -1,27 +1,37 @@
 import React from "react";
-import { Image, View } from "react-native";
+import { StyleSheet, Image, View } from "react-native";
 import { Text, Card, Button, Icon } from "react-native-elements";
 
-const MyCard = ({ item }) => {
+const styles = StyleSheet.create({
+  buttonStyle:{
+    borderRadius: 0,
+    marginLeft: 0,
+    marginRight: 0,
+    marginBottom: 0,
+  },
+  imageStyle:{
+    width: 100, 
+    height: 75, 
+    marginBottom: 10,
+  },
+  viewStyle:{
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+const MyCard = ({ item, subscribe, unsubscribe }) => {
   return (
     <Card
       title={item.name}
       // image={item.image}
       // image={require('../images/pic2.jpg')}>
     >
-      <View>
-        {item.image && (
-          <Image resizeMode="cover" source={{ uri: item.image }} />
-        )}
-        <Text style={{ marginBottom: 10 }}>{item.name}</Text>
-        <Button
+      <View style={styles.viewStyle}>
+      <Image source={{uri: item.image}}
+       style={styles.imageStyle} />
+        <Button style ={styles.buttonStyle}
           icon={<Icon name="code" color="#ffffff" />}
-          buttonStyle={{
-            borderRadius: 0,
-            marginLeft: 0,
-            marginRight: 0,
-            marginBottom: 0,
-          }}
           title="VIEW NOW"
         />
       </View>
